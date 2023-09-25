271a3d89b70f53bfe197885cae98c8f9ac994102
Marcelo Bento <88855039+B12hj89@users.noreply.github.com>
2023-09-25 16:27:12 -0300

    Add files via upload
    Teoria e Resolução do SetInterface

---
+++ collections-java-api-2023-exercio/src/PesquisaSet/Contato.java
@@ -0,0 +1,52 @@
+package PesquisaSet;
+
+import java.util.Objects;
+
+public class Contato {
+    public Object setnumero;
+    //atriutos
+
+    private String nome;
+
+    private int numero;
+
+    public Contato(String nome, int numero) {
+        this.nome = nome;
+        this.numero = numero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public Contato(int numero) {
+        this.numero = numero;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Contato contato)) return false;
+        return Objects.equals(getNome(), contato.getNome());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNome());
+    }
+
+    @Override
+    public String toString() {
+        return "Contato{" +
+                "nome='" + nome + '\'' +
+                ", numero=" + numero +
+                '}';
+    }
+
+    public void setnumero(int novoNumero) {
+    }
+}
