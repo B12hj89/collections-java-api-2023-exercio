ce8a088fbd495e419320fa34b8a6151ab7d82076
Marcelo Bento <88855039+B12hj89@users.noreply.github.com>
2023-09-26 17:53:16 -0300

    Add files via upload
    Resolvendo Exercícios Collection Set Interface, Map Interface

---
+++ collections-java-api-2023-exercio/collections-java-api-2023-exercio/src/Map/AgendaContatos.java
@@ -0,0 +1,69 @@
+package Map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AgendaContatos {
+    //atributos
+    private Map<String, Integer> agendaContatoMap;
+
+    public AgendaContatos() {
+        this.agendaContatoMap = new HashMap<>();
+    }
+
+    public void adicionarContato(String nome, Integer telefone){
+        agendaContatoMap.put(nome, telefone);
+    }
+    public void removerContato(String nome){
+        if(!agendaContatoMap.isEmpty()){
+            agendaContatoMap.remove(nome);
+        }
+
+
+
+
+
+    }
+
+    public void exibirContatos(){
+        System.out.println(agendaContatoMap);
+    }
+
+
+
+    public Integer pesquisarPorNome(String nome){
+        Integer numeroPornome = null;
+        if(!agendaContatoMap.isEmpty()){
+           numeroPornome = agendaContatoMap.get(nome);
+        }
+        return numeroPornome;
+    }
+
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        // Adicionar contatos
+        agendaContatos.adicionarContato("Camila", 123456);
+        agendaContatos.adicionarContato("João", 5665);
+        agendaContatos.adicionarContato("Carlos", 1111111);
+        agendaContatos.adicionarContato("Ana", 654987);
+        agendaContatos.adicionarContato("Maria", 1111111);
+        agendaContatos.adicionarContato("Camila", 44444);
+
+        agendaContatos.exibirContatos();
+
+        // Remover um contato
+        agendaContatos.removerContato("Maria");
+        agendaContatos.exibirContatos();
+
+        // Pesquisar número por nome
+        String nomePesquisa = "João";
+        Integer numeroPesquisa = agendaContatos.pesquisarPorNome("João");
+        System.out.println("Número de telefone de " + nomePesquisa + ": " + numeroPesquisa);
+
+        String nomePesquisaNaoExistente = "Paula";
+        Integer numeroPesquisaNaoExistente = agendaContatos.pesquisarPorNome(nomePesquisaNaoExistente);
+        System.out.println("Número de telefone de " + nomePesquisaNaoExistente + ": " + numeroPesquisaNaoExistente);
+
+    }
+}
---
+++ collections-java-api-2023-exercio/collections-java-api-2023-exercio/src/OperacoesBasicasSet/ConjuntoConvidados.java
@@ -0,0 +1,69 @@
+package OperacoesBasicasSet;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ConjuntoConvidados {
+    //atributo
+
+    private Set<Convidado> convidadoSet;
+
+    public ConjuntoConvidados() {
+        this.convidadoSet = new HashSet<>();
+
+    }
+
+    public void adicionarConvidado(String nome, int codigoConvite){
+
+        convidadoSet.add(new Convidado(nome, codigoConvite));
+    }
+
+    public void removerConvidadoPorCodigoConvite(int codigoConvite){
+
+        Convidado convidadoParaRemover = null;
+        for(Convidado c : convidadoSet) {
+            if(c.getCodigoConvite() == codigoConvite) {
+                convidadoParaRemover = c;
+                break;
+            }
+
+        }
+        convidadoSet.remove(convidadoParaRemover);
+
+    }
+
+    public int contarConvidados(){
+        return convidadoSet.size();
+    }
+
+    public void exibirConvidados(){
+        System.out.println(convidadoSet);
+    }
+
+    public static void main(String[] args) {
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+        // Exibindo o número de convidados no conjunto (deve ser zero)
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de Convidados");
+
+        conjuntoConvidados.adicionarConvidado("Convidado 1", 1234);
+        conjuntoConvidados.adicionarConvidado("Convidado 2", 1235);
+        conjuntoConvidados.adicionarConvidado("Convidado 3", 1235);
+        conjuntoConvidados.adicionarConvidado("Convidado 4", 1236);
+
+        conjuntoConvidados.exibirConvidados();
+
+
+        // Exibindo os convidados no conjunto
+        System.out.println("Convidados no conjunto:");
+        conjuntoConvidados.exibirConvidados();
+
+        // Exibindo os convidados no conjunto
+        System.out.println("Convidados no conjunto:");
+        conjuntoConvidados.exibirConvidados();
+    }
+}
+
+
+
+
